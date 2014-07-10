c90187bfbee1b794b2e3cb1c6895be21e9483ce9
Brittany Wald <bwald@paperlesspost.com>
2014-07-10 17:43:46 -0400

    get endpoint & method

--- github.py
+++ github.py
@@ -5,7 +5,7 @@ Makes sure API calls are happening in the background to prevent lag
 Uses GitHub API to grab pull requests
 '''
 class GitHubApi(threading.Thread):
-    def __init__(self, base_uri="https://api.github.com", token=None, pull_request, string, timeout):
+    def __init__(self, base_uri="https://api.github.com/repos/", token=None, pull_request, string, timeout):
         self.pull_request = pull_request
         self.original = string
         self.timeout = timeout
@@ -22,3 +22,10 @@ class GitHubApi(threading.Thread):
             err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
         except (urllib2.URLError) as (e):
             err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+
+    def get(self, endpoint, params=None):
+        return self.request('get', endpoint, params=params)
+
+    def get_pull_request(self, pull_request):
+        data = self.get(base_uri + username + "/pulls" + pull_request + "/files")
+        return list(data["files"].values())[0]["content"]
