c058a59eb7a3ac6eb8d8faf359eb40bfb6f0fd70
Brittany Wald <bwald@paperlesspost.com>
2014-07-10 18:21:02 -0400

    update init & request method for api

--- github.py
+++ github.py
@@ -5,11 +5,11 @@ Makes sure API calls are happening in the background to prevent lag
 Uses GitHub API to grab pull requests
 '''
 class GitHubApi(threading.Thread):
-    def __init__(self, base_uri="https://api.github.com/repos/", token=None, pull_request, string, timeout):
-        self.pull_request = pull_request
-        self.original = string
+    def __init__(self, base_uri="https://api.github.com/repos/", token=None, timeout, username):
+        self.base_uri = base_uri
+        self.token = token
         self.timeout = timeout
-        self.result = None
+        self.username = username
         threading.Thread.__init__(self)
 
     def get(self, endpoint, params=None):
@@ -18,8 +18,4 @@ class GitHubApi(threading.Thread):
     def get_pull_request(self, pull_request):
         try:
             data = json.load(urllib2.urlopen(base_uri + username + "/pulls" + pull_request + "/files"))
-            return list(data["files"].values())[0]["content"]           
-        except (urllib2.HTTPError) as (e):
-            err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
-        except (urllib2.URLError) as (e):
-            err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+            return data["filename"]
