ffaa1553c3f9b54f25f8e41dd101eaa67cce133b
Ivan Tse <ivan.tse1@gmail.com>
2014-07-11 11:15:05 -0400

    remove hardcoded strings

--- spoke.py
+++ spoke.py
@@ -7,18 +7,28 @@ import github
 import json
 import urllib
 import urllib2
+import re
 
 class Spoke(sublime_plugin.TextCommand):
     def run(self, edit):
-        # sublime
-        # sublime.active_window().open_file('/Users/ivantse/Sites/paperlesspost/paperless-post/app/controllers/photo_albums_controller.rb')
-        
-        self.view.window().show_input_panel("Github Pull Request:", '', self.on_done, None, None)
+        if self.run_command(['git', 'rev-parse', '--is-inside-work-tree']).strip() == 'true':
+            self.remote = 'root'
+            remote_url = self.run_command(['git', 'config', '--get', 'remote.'+self.remote+'.url']).strip()
+            if not remote_url:
+                self.remote = 'origin'
+                remote_url = self.run_command(['git', 'config', '--get', 'remote.'+self.remote+'.url']).strip()
+            print remote_url
+            match = re.search('(\w*)[/:]([\w-]*).git\Z', remote_url)
+            self.username = match.group(1)
+            self.repo = match.group(2)
+            self.view.window().show_input_panel("Github Pull Request:", '', self.on_done, None, None)
+        else:
+            sublime.error_message('Not in a git directory')
     
     def on_done(self, pull_request_id):
-        github = GitHubApi('paperlesspost')
-        self.run_command(['git', 'fetch', 'root', 'pull/'+pull_request_id+'/head:root/pull/'+pull_request_id+'/head'])
-        self.run_command(['git', 'checkout', 'root/pull/'+pull_request_id+'/head'])
+        github = GitHubApi(self.username, self.repo)
+        self.run_command(['git', 'fetch', self.remote, 'pull/'+pull_request_id+'/head:'+self.remote+'/pull/'+pull_request_id+'/head'])
+        self.run_command(['git', 'checkout', self.remote+'/pull/'+pull_request_id+'/head'])
         files = github.get_pull_request(pull_request_id)
         for f in files:
             sublime.active_window().open_file(sublime.active_window().folders()[0] + "/" + f)
@@ -34,21 +44,24 @@ class Spoke(sublime_plugin.TextCommand):
         proc = subprocess.Popen(args, stdout=subprocess.PIPE,
                                 startupinfo=startupinfo, stderr=subprocess.PIPE, cwd=sublime.active_window().folders()[0])
         output = proc.stdout.read()
+        print output
         return output
 
 '''
 Uses GitHub API to grab pull requests
 '''
 class GitHubApi():
-    def __init__(self, username, base_uri="https://api.github.com/repos/", token=None):
+    def __init__(self, username, repo, base_uri="https://api.github.com/repos/", token=None):
         self.base_uri = base_uri
         self.token = token
         self.username = username
+        self.repo = repo
 
     def get_pull_request(self, pull_request):
         files = []
-        # print(self.base_uri + self.username + "/paperless-post" + "/pulls/" + pull_request + "/files?access_token=69ebc1c048e4858175bfd65835eebbcb78102705")
-        data = json.load(urllib2.urlopen(self.base_uri + self.username + "/paperless-post" + "/pulls/" + pull_request + "/files?access_token=69ebc1c048e4858175bfd65835eebbcb78102705"))
+        url = self.base_uri + self.username + "/" + self.repo + "/pulls/" + pull_request + "/files?access_token=d5add3a1f915b745a73622e3f6b7b1fc4b7e1bce"
+        print url
+        data = json.load(urllib2.urlopen(url))
         for f in data:
             files.append(f['filename'])
         return files
