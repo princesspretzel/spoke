cfd9e3c087b2bfee8980256c2397707b5595e13d
Ivan Tse <ivan.tse1@gmail.com>
2014-07-11 13:00:08 -0400

    threading

--- spoke.py
+++ spoke.py
@@ -26,25 +26,19 @@ class Spoke(sublime_plugin.TextCommand):
             sublime.error_message('Not in a git directory')
     
     def on_done(self, pull_request_id):
-        github = GitHubApi(self.username, self.repo)
+        github = GitHubApi(self.username, self.repo, pull_request_id)
         self.run_command(['git', 'fetch', self.remote, 'pull/'+pull_request_id+'/head:'+self.remote+'/pull/'+pull_request_id+'/head'])
         self.run_command(['git', 'checkout', self.remote+'/pull/'+pull_request_id+'/head'])
-        files = github.get_pull_request(pull_request_id)
-        for f in files:
-            sublime.active_window().open_file(sublime.active_window().folders()[0] + "/" + f)
-        print(files)
-
+        github.start()
 
     def run_command(self, args):
         startupinfo = None
-        print sublime.active_window().folders()[0]
         if os.name == 'nt':
             startupinfo = subprocess.STARTUPINFO()
             startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         proc = subprocess.Popen(args, stdout=subprocess.PIPE,
                                 startupinfo=startupinfo, stderr=subprocess.PIPE, cwd=sublime.active_window().folders()[0])
         output = proc.stdout.read()
-        print output
         return output
 
 '''
@@ -52,21 +46,23 @@ Uses GitHub API to grab pull requests
 Extends threading to run API calls in the background & prevent lag
 '''
 class GitHubApi(threading.Thread):
-    def __init__(self, username, repo, base_uri="https://api.github.com/repos/", token=None):
+    def __init__(self, username, repo, pull_request_id, base_uri="https://api.github.com/repos/", token=None):
         self.base_uri = base_uri
         self.token = token
         self.username = username
         self.repo = repo
+        self.pull_request_id = pull_request_id
         threading.Thread.__init__(self)
 
-    def get_pull_request(self, pull_request):
+    def get_pull_request(self):
         files = []
-        url = self.base_uri + self.username + "/" + self.repo + "/pulls/" + pull_request + "/files?access_token=d5add3a1f915b745a73622e3f6b7b1fc4b7e1bce"
+        url = self.base_uri + self.username + "/" + self.repo + "/pulls/" + self.pull_request_id + "/files?access_token=d5add3a1f915b745a73622e3f6b7b1fc4b7e1bce"
         print url
         data = json.load(urllib2.urlopen(url))
         for f in data:
             files.append(f['filename'])
-        return files
+        for f in files:
+            sublime.set_timeout(lambda f=f: sublime.active_window().open_file(sublime.active_window().folders()[0] + "/" + f), 0)
 
-thread = GitHubApi(target=get_pull_request, args=(pull_request))
-thread.start()
+    def run(self):
+        self.get_pull_request()
